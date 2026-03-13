cd1b020ee0d95b292c9c8a1aafe509a999db3aa2
yuuuuuuanhua <161398282+yuuuuuuanhua@users.noreply.github.com>
2026-03-13 18:02:19 +0900

    feat: add barrel export for all modules

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * ts-utils-toolkit
+ * A collection of practical TypeScript utility functions
+ */
+
+export * from './string'
+export * from './array'
+export * from './object'
+export * from './async'
+export * from './date'
+export * from './validator'
+export * from './number'
